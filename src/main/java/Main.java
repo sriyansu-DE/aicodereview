422596507591ef23aacfb097587358c12a189f7c
Abhijit Kumar <abhijitkumar1907@gmail.com>
2026-09-04 10:50:41 +0530

    Initial AI code reviewer implementation

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String code = """
+                public class Test {
+                    public void hello() {
+                        System.out.println("Hello");
+                    }
+                }
+                """;
+
+        CompilationUnit cu = StaticJavaParser.parse(code);
+
+        System.out.println(cu);
+    }
+}
